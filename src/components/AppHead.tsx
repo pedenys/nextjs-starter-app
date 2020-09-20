ff503586a70f497d987ce9acc82fe216c0922968
Pierre-Emmanuel DENYS <pedenys@pm.me>
2020-09-20 17:46:59 +0200

    Update _document page and Apphead component

--- src/components/AppHead.tsx
+++ src/components/AppHead.tsx
@@ -6,16 +6,8 @@ type AppHeadProps = {
 
 const AppHead: React.FC<AppHeadProps> = ({ title }) => (
   <Head>
-    {title && <title>{title}</title>}
     <meta charSet="utf-8" />
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <meta name="theme-color" content="#EFEFEF"></meta>
-    <link rel="manifest" href="/manifest.json" />
-    {/* TODO : add favicons */}
-    {/* <link rel="shortcut icon" href="./somewhere" /> */}
-    {/* <link rel="apple-touch-icon" href="./somewhere" /> */}
-    {/* TODO : update meta description */}
-    <meta name="description" content=""></meta>
+    {title && <title>{title}</title>}
   </Head>
 )
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,25 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import AppHead from '../components/AppHead'
 
 export default class MyDocument extends Document<any> {
   render() {
     return (
-      <html>
-        <AppHead />
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#EFEFEF"></meta>
+          <link rel="manifest" href="/manifest.json" />
+          {/* TODO : add favicons */}
+          {/* <link rel="shortcut icon" href="./somewhere" /> */}
+          {/* <link rel="apple-touch-icon" href="./somewhere" /> */}
+          {/* TODO : update meta description */}
+          <meta name="description" content=""></meta>
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
